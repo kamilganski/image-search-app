ce489ecca488f8e224abb18b118ab055fc60ef62
Kamil Ganski <kamilganski2@gmail.com>
2020-07-09 15:06:00 +0200

    Add layout for search tab, add controller for search images

--- src/aura/NGUR_ImagesSearch/NGUR_ImagesSearchController.js
+++ src/aura/NGUR_ImagesSearch/NGUR_ImagesSearchController.js
@@ -2,4 +2,8 @@
     onInit: function (component, event, helper) {
         helper.doInit(component, event);
     },
+
+    onSearchImages: function (component, event, helper) {
+        helper.searchImages(component, event);
+    }
 });
--- src/aura/NGUR_ImagesSearch/NGUR_ImagesSearchHelper.js
+++ src/aura/NGUR_ImagesSearch/NGUR_ImagesSearchHelper.js
@@ -1,5 +1,30 @@
 ({
     doInit: function (component, event) {
-
+        component.set("v.columns", [
+            {label: "Title", fieldName: "NGUR_Title__c", type: "text"},
+            {label: "Image URL", fieldName: "NGUR_Image_Url__c", type: "url"},
+            {label: "Thumbnail URL", fieldName: "NGUR_Thumbnail_Url__c", type: "url"}
+        ]);
     },
+
+    searchImages: function (component, event) {
+        let title = component.get("v.title");
+        let action = component.get("c.searchImagesByTitle");
+
+        action.setParams({
+            title: title
+        });
+        action.setCallback(this, function (response) {
+            let state = response.getState();
+
+            if (state === "SUCCESS") {
+                let data = response.getReturnValue();
+
+                component.set("v.searchedImages", data);
+            } else {
+                console.log('error');
+            }
+        });
+        $A.enqueueAction(action);
+    }
 });
